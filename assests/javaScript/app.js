78a67074238a29ee299bd7a3db9fc35f1ef2d25f
Ryan Evans <Fatkid@Ryans-MacBook-Pro.local>
2018-01-07 22:31:42 -0800

    pseudo coding javascript linking external pages

---
+++ assests/javaScript/app.js
@@ -0,0 +1,20 @@
+// checking if javaScript page linked
+// alert("hello");
+
+// checking if jQuery CDN is working
+// $("body").html("hello world");
+
+// start of javaScript/jQuery code
+
+// connect to giphy API
+
+// push buttons from giphy API to header in html
+
+// append images to container for main content
+// limit to 10 images
+// show image ratings
+
+
+// create a 'query' user input side bar to create new buttons of animals user types
+
+// on click change images to match animal click and append to container area
