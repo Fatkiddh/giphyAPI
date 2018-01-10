ee71a4b906cd99bb8d707a835c1a1774ca4489db
Ryan Evans <Fatkid@Ryans-MacBook-Pro.local>
2018-01-09 22:48:29 -0800

    wrote html, css, logic

--- assests/javaScript/app.js
+++ assests/javaScript/app.js
@@ -6,15 +6,93 @@
 
 // start of javaScript/jQuery code
 
-// connect to giphy API
 
-// push buttons from giphy API to header in html
+var Animals = ['dog ', 'cat ', 'rabbit', 'skunk', 'goldfish', 'bird', 'ferret', 'turtle', 'sugar glider', 'chinchilla', 'hedgehog', 'hermit crab', 'gerbil', 'pygmy goat',
+'chick', 'capybara', 'teacup pig', 'serval', 'salamander', 'frog',];
 
-// append images to container for main content
-// limit to 10 images
-// show image ratings
+function renderButtons() {
+
+      $('#animals-buttons').empty();
+
+      for (var i = 0; i < Animals.length; i++) {
+
+        var a = $('<button>');
+
+        a.addClass("animal");
+
+        a.attr('data-name', Animals[i]);
+
+        a.text(Animals[i]);
+
+        $("#animals-buttons").append(a);
+      }
+  }
+
+  $("#add-animal").on('click', function(event) {
+
+    event.preventDefault();
+
+    var animals = $("#animalInput").val().trim();
+
+    Animals.push(Animals);
 
 
-// create a 'query' user input side bar to create new buttons of animals user types
+  });
 
-// on click change images to match animal click and append to container area
+
+  renderButtons();
+
+  $("button").on('click',  function(){
+
+        var animal = $(this).attr('data-name');
+
+        var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + Animals + "&api_key=xdEyfZIyHfHB6ilBXQHP4T6bcwSi5bua&limit=12";
+
+        $.ajax({
+          url: queryURL,
+          method: "GET"
+        }).done(function(response){
+          var results = response.data;
+          console.log(response);
+
+          for (var i = 0; i < results.length; i++) {
+
+            if (results[i].rating !== "pg" && results[i].rating !== 'pg-14') {
+
+              var gifDiv = $("<div class = 'item'>");
+
+              var rating = results[i].rating;
+
+              var p = $("<p>").text("Rating: " + rating);
+
+              var animalImage = $("<img>");
+
+              animalImage.attr("src", results[i].images.fixed_height.url);
+              animalImage.attr('data-state', 'animate');
+              animalImage.attr('data-still', results[i].images.fixed_height_still.url);
+              animalImage.attr("data-anamate", results[i].images.fixed_height.url);
+
+              gifDiv.append(p);
+              gifDiv.append(animalImage);
+
+              $("#gifs-appear-here").prepend(gifDiv);
+
+            }
+          }
+        });
+
+  });
+
+
+        $('gifDiv').on('click', function(){
+            var state = $(this).attr("data-state");
+
+            if (state === "still") {
+              $(this).attr('src', $(this).attr('data-animate'));
+              $(this).attr("data-state", "animate");
+            }
+            else {
+              $(this).attr("src", $(this).attr('data-animate'));
+              $(this).attr("data-stae", "still");
+            }
+        });
